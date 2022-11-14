08bbc92bd03ec93acf55f810d92cd48f65b54d3a
Yong <hopeyongjun421@gmail.com>
2022-11-13 20:12:01 -0500

    add: project class

--- src/projects/ProjectPage.tsx
+++ src/projects/ProjectPage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { MOCK_PROJECTS } from "./MockProjects";
 
 function ProjectPage() {
-    return <h1>Projects</h1>;
+    return (
+        <>
+            <h1>Projects</h1>
+            <pre>{JSON.stringify(MOCK_PROJECTS, null, '')}</pre>
+        </>
+    );
 }
 
 export default ProjectPage;
